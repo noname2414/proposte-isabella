a6d8959d7c7529272938519fa1aef5d4fe8a58d3
noname2414 <kurosaki.onepiece@gmail.com>
2024-08-13 23:12:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.yes-btn').addEventListener('click', function() {
+    alert("¡Genial! Estoy emocionado/a por lo que viene. 😊");
+});
+
+document.querySelector('.yes-btn').addEventListener('click', function() {
+    alert("si o si no hay otra opcion y te amo 😢");
+});
